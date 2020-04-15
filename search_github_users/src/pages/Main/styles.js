0370fb5d81c788ed9bc3052aaf800faa48f01956
matheus.goncalves <matheusvieira1900@hotmail.com>
2020-04-15 13:15:53 -0300

    Estilizando a pagina main

--- search_github_users/src/pages/Main/styles.js
+++ search_github_users/src/pages/Main/styles.js
@@ -1,6 +1,36 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
   padding: 30px;
+`;
+
+export const Form = styled.View`
+  flex-direction: row;
+  padding-bottom: 20px;
+  border-bottom-width: 5px;
+  border-color: #eee;
+  box-shadow: 10px 5px 5px black;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: '#999',
+})`
+  flex: 1;
+  height: 40px;
+  background: #eee;
+  border-radius: 4px;
+  padding: 0 15px;
+  border: 1px solid #eee;
+`;
+
+// Foi colocado dentro de () pois é um componente de outra biblioteca
+export const SubmitButton = styled(RectButton)`
+  background: #7159c1;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 12px;
 `;
